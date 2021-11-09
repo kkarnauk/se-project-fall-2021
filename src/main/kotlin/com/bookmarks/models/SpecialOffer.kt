d27120960147ea6e1ec0da90f12f67141ee58068
Lebedev Egor <egorikl@list.ru>
2021-11-09 18:49:59 +0300

    Add special offers

--- src/main/kotlin/com/bookmarks/models/SpecialOffer.kt
+++ src/main/kotlin/com/bookmarks/models/SpecialOffer.kt
@@ -2,4 +2,4 @@ package com.bookmarks.models
 
 import java.util.*
 
-data class SpecialOffer(val deadline: Date, val discount: Double, val freeRentWeekBookIds: List<UInt> = listOf())
+data class SpecialOffer(val deadline: Date, val discount: Double)
