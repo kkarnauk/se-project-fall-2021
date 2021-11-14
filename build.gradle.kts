b8d775a707a42fd1f81361968ec554eb6422007c
peter <petyo333@gmail.com>
2021-11-14 23:39:31 +0300

    Fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,28 +2,23 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.5.5"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0" apply true
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
     id("jacoco")
 }
 
 group = "com.bookmarks"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:2.5.6")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    implementation("org.jacoco:org.jacoco.core:0.8.7")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6")
-    implementation("org.jetbrains.exposed:spring-transaction:0.36.2")
+    implementation("org.springframework.boot:spring-boot-starter:2.5.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.5")
 }
 
 ktlint {
