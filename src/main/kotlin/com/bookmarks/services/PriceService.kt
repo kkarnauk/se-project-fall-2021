b447f63163d8184320c1a677b84bf0b6d62c8ed1
Kirill <bitikit@gmail.com>
2021-11-14 22:58:27 +0300

    Add missing new lines at the end of files

--- src/main/kotlin/com/bookmarks/services/PriceService.kt
+++ src/main/kotlin/com/bookmarks/services/PriceService.kt
@@ -1,45 +1,15 @@
 package com.bookmarks.services
 
-import com.bookmarks.models.*
+import com.bookmarks.models.Book
+import com.bookmarks.models.Price
 import com.bookmarks.models.Price.Companion.fromCents
 import com.bookmarks.models.Price.Companion.toCents
-import com.bookmarks.services.PriceService.applySpecialOffer
-import java.util.*
+import com.bookmarks.models.User
 import org.springframework.stereotype.Component
 
 @Component
 object PriceService {
-    fun calculateCartPrice(user: User, cart: Cart, specialOffer: SpecialOffer? = null): Price? {
-        if (cart.bookList().groupBy { it }.filterValues { it.size > 1 }.isNotEmpty()) return null
-        if (cart.bookList().any { it.id in user.purchasedBookIds }) return null
-
-        val purchases = cart.elements.filterIsInstance<Purchase>().map { it.book }
-        val rents = cart.elements.filterIsInstance<Rent>()
-
-        val rentPrice = rents.fold(Price(0, 0)) { acc, rent ->
-            val rentWeekPrice = calculateWeekRentPrice(user, rent.book, specialOffer) ?: return null
-            acc + (rentWeekPrice.toCents() * rent.weeks).fromCents()
-        }
-
-        val purchasePrice =
-            if (purchases.isEmpty()) 0.fromCents()
-            else calculatePurchasePrice(user, purchases, specialOffer) ?: return null
-
-        return rentPrice + purchasePrice
-    }
-
-//    Full rent price if half of purchase price, scales to week linearly
-    fun calculateWeekRentPrice(user: User, book: Book, specialOffer: SpecialOffer? = null): Price? =
-        if (specialOffer?.freeRentWeekBookIds?.contains(book.id) == true) {
-            0.fromCents()
-        } else calculatePurchasePriceBase(user, book)?.let {
-            (it.toCents() * 7.0 / book.readDays * 0.5).toInt().fromCents().applySpecialOffer(specialOffer)
-        }
-
-    fun calculatePurchasePrice(user: User, book: Book, specialOffer: SpecialOffer? = null): Price? =
-        calculatePurchasePriceBase(user, book)?.applySpecialOffer(specialOffer)
-
-    private fun calculatePurchasePriceBase(user: User, book: Book): Price? {
+    fun calculatePurchasePrice(user: User, book: Book): Price? {
         if (book.id in user.purchasedBookIds) {
             return null
         }
@@ -49,12 +19,9 @@ object PriceService {
             .minOrNull() ?: book.basePrice
     }
 
-    fun calculatePurchasePrice(user: User, books: List<Book>, specialOffer: SpecialOffer? = null): Price? =
-        calculatePurchasePriceBase(user, books)?.applySpecialOffer(specialOffer)
-
-    private fun calculatePurchasePriceBase(user: User, books: List<Book>): Price? = books
+    fun calculatePurchasePrice(user: User, books: List<Book>): Price? = books
         .ifEmpty { return null }
-        .map { calculatePurchasePriceBase(user, it) }
+        .map { calculatePurchasePrice(user, it) }
         .ifNullsIn { return null }
         .sumOf { it.toCents() }
         .fromCents() * when {
@@ -67,11 +34,4 @@ object PriceService {
     private inline fun <T> List<T?>.ifNullsIn(ifInDo: () -> List<T>): List<T> {
         return if (null in this) ifInDo() else this as List<T>
     }
-
-    private fun Price.applySpecialOffer(specialOffer: SpecialOffer?): Price =
-        specialOffer?.let {
-            if (it.deadline.after(Date())) this * it.discount else this
-        } ?: this
-
-    private fun Cart.bookList(): List<Book> = this.elements.map { it.book }
 }
